4114401c3b0f28598839becf6ec9e0992e3f146f
thiagossmarins <thiagodesouzasallesmarins@gmail.com>
2024-04-01 11:51:02 -0300

    Configuracoes iniciais

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-x1 font-bold text-red-500'>Hello tailwindcss with react</h1>
+    </>
+  )
+}
+
+export default App
